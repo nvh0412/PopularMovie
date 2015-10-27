099b310ddff08a7a3b040da221beeb069ca295c4
Hoa Nguyen <nvh0412@gmail.com>
2015-10-27 13:54:37 +0700

    [Data Sprint] Completed test
    testCreateDB and testMovideDb

--- app/src/androidTest/java/com/udacity/hoanv/popularmovie/data/TestDB.java
+++ app/src/androidTest/java/com/udacity/hoanv/popularmovie/data/TestDB.java
@@ -1,6 +1,11 @@
 package com.udacity.hoanv.popularmovie.data;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
+
+import java.util.HashSet;
 
 /**
  * Created by HoaNV on 10/26/15.
@@ -30,16 +35,84 @@ public class TestDB extends AndroidTestCase{
     public void testCreateDatabase() throws Throwable{
         MovieDBHelper dbHelper = new MovieDBHelper(mContext);
 
+        final HashSet<String> tableNameSet = new HashSet<>();
+        tableNameSet.add(MovieContract.MovieEntry.TABLE_NAME);
+
+        //Check db open
+        SQLiteDatabase sqLiteDatabase = dbHelper.getReadableDatabase();
+        assertTrue("DB should be open", sqLiteDatabase.isOpen());
+
         //Check table name.
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT name FROM sqlite_master WHERE type = 'table'", null);
+        assertTrue("Cursor should be can move to first row", cursor.moveToFirst());
+
+        do{
+            tableNameSet.remove(cursor.getString(0));
+        }while(cursor.moveToNext());
+
+        assertTrue("TableNameSet should be empty", tableNameSet.isEmpty());
 
         //Check column name.
+        cursor = sqLiteDatabase.rawQuery("PRAGMA table_info(" + MovieContract.MovieEntry.TABLE_NAME + ")", null);
+
+        final HashSet<String> columnNameSet = new HashSet<String>();
+        columnNameSet.add(MovieContract.MovieEntry._ID);
+        columnNameSet.add(MovieContract.MovieEntry.COLUMN_MOVIE_ID);
+        columnNameSet.add(MovieContract.MovieEntry.COLUMN_TITLE);
+        columnNameSet.add(MovieContract.MovieEntry.COLUMN_OVERVIEW);
+        columnNameSet.add(MovieContract.MovieEntry.COLUMN_RELEASE_DATE);
+        columnNameSet.add(MovieContract.MovieEntry.COLUMN_POSTER_PATH);
+
+        int columnNameIndex = cursor.getColumnIndex("name");
+        do{
+            columnNameSet.remove(cursor.getString(columnNameIndex));
+        }while(cursor.moveToNext());
+
+        assertTrue("Error: The datase doesn't contain all of the required movie entry columns",
+                columnNameSet.isEmpty());
+
+        cursor.close();
+        dbHelper.close();
     }
 
     /**
      *
      */
-    public void testMovideTable(){
+    public void testMovideTable() {
+        MovieDBHelper dbHelper = new MovieDBHelper(mContext);
 
+        //First step: Get reference to writable database
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        assertTrue("DB should be opened!", db.isOpen());
+
+        //Second Step: Create contentValues for save into database
+        ContentValues testValues = TestUtilities.createMovieValues();
+
+
+        //Third Step: Insert into database
+        long id = db.insert(MovieContract.MovieEntry.TABLE_NAME, null, testValues);
+        assertTrue("Id of row should be different with -1", id != -1);
+
+        //Four Step: Query and check ID
+        Cursor cursor = db.query(MovieContract.MovieEntry.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        assertTrue("Cursor should be not empty", cursor.moveToFirst());
+
+        //Fifth step: Validate data from db with contentValues
+        TestUtilities.validateCurrentRecord(cursor, testValues);
+
+        //Check only this record in database.
+        assertFalse("More than one records returned from location query", cursor.moveToNext());
+
+        //Close connection
+        cursor.close();
+        db.close();
     }
 
     @Override
--- app/src/androidTest/java/com/udacity/hoanv/popularmovie/data/TestUtilities.java
+++ app/src/androidTest/java/com/udacity/hoanv/popularmovie/data/TestUtilities.java
@@ -1,7 +1,18 @@
 package com.udacity.hoanv.popularmovie.data;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+
 /**
  * Created by HoaNV on 10/26/15.
  */
 public class TestUtilities {
+
+    public static ContentValues createMovieValues() {
+        return null;
+    }
+
+    public static void validateCurrentRecord(Cursor cursor, ContentValues testValues) {
+
+    }
 }
